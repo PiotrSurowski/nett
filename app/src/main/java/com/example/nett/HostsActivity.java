2b169cfda21a16c588fb3939c82a05a793e4bc5f
paulinazaw <paulinazawislak1@gmail.com>
2022-04-24 20:06:09 +0200

    new activity and layout "Skanowanie zdalnego hosta"

---
+++ app/src/main/java/com/example/nett/HostsActivity.java
@@ -0,0 +1,12 @@
+package com.example.nett;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HostsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hosts_layout);
+    }
+}

8fb261d13c92c524e88901460bdfba988f3c9eed
paulinazaw <paulinazawislak1@gmail.com>
2022-04-24 19:54:19 +0200

    new activity - "Adresacja IP"

---
+++ app/src/main/java/com/example/nett/IpActivity.java
@@ -0,0 +1,13 @@
+package com.example.nett;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ip_layout);
+    }
+}

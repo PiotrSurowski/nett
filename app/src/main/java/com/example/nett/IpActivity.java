1d826ee75e64212ac5897eabd6f5b104146a02ad
paulinazaw <paulinazawislak1@gmail.com>
2022-04-26 11:12:05 +0200

    IP layout - commit

--- app/src/main/java/com/example/nett/IpActivity.java
+++ app/src/main/java/com/example/nett/IpActivity.java
@@ -2,6 +2,9 @@ package com.example.nett;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class IpActivity extends Activity {
 
@@ -9,5 +12,7 @@ public class IpActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ip_layout);
+
     }
+
 }

90d7ad9473189a517bc980bcf86617781a23553d
paulinazaw <paulinazawislak1@gmail.com>
2022-04-27 21:28:28 +0200

    nav menu

--- app/src/main/java/com/example/nett/MainActivity.java
+++ app/src/main/java/com/example/nett/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -15,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
 
         ImageButton im_ip = (ImageButton)findViewById(R.id.ip);
@@ -50,4 +51,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 }

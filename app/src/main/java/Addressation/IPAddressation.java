4cf42cd24b4adc0c079a24e97b7688952e86a78a
piotrsurowski <piotr.surowski248@gmail.com>
2022-04-30 22:57:30 +0100

    Backend <--> Frontend (IpActivity)

--- app/src/main/java/Addressation/IPAddressation.java
+++ app/src/main/java/Addressation/IPAddressation.java
@@ -4,12 +4,35 @@ import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NonNull;
 
 @AllArgsConstructor
-@Getter
 public class IPAddressation {
+    @NonNull
     private List<Integer> address;
+    @NonNull
     private List<Integer> mask;
+    @NonNull
     private List<Integer> networkAddress;
+    @NonNull
     private List<Integer> broadcastAddress;
+
+    public IPAddressation(List<Integer> address, List<Integer> mask, List<Integer> networkAddress, List<Integer> broadcastAddress) {
+    }
+
+    public List<Integer> getAddress() {
+        return address;
+    }
+
+    public List<Integer> getMask() {
+        return mask;
+    }
+
+    public List<Integer> getNetworkAddress() {
+        return networkAddress;
+    }
+
+    public List<Integer> getBroadcastAddress() {
+        return broadcastAddress;
+    }
 }

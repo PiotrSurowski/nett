ee7175898f06c779ed7b0337e7185ec176aa74cc
piotrsurowski <piotr.surowski248@gmail.com>
2022-05-01 20:19:46 +0100

    Backend <--> Frontend (HostActivity) Internet permission added to manifest.

--- app/src/main/java/IPScanner/LocalPortScanner.java
+++ app/src/main/java/IPScanner/LocalPortScanner.java
@@ -1,5 +1,7 @@
 package IPScanner;
 
+import android.content.Context;
+
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -8,13 +10,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class PortScannerManager extends PortScanner{
+public class LocalPortScanner extends PortScanner{
     private final Address address;
     private final List<String> addressRange;
     private final Map<String, List<Integer>> results;
 
 
-    public PortScannerManager(Integer startPort, Integer endPort) throws UnknownHostException {
+    public LocalPortScanner(Integer startPort, Integer endPort) throws UnknownHostException {
         super(startPort, endPort);
         address = new Address();
         results = new HashMap<>();
@@ -26,14 +28,18 @@ public class PortScannerManager extends PortScanner{
     }
 
     private void runTask() throws InterruptedException, IOException {
+        float counter = 0;
         for (String s : addressRange){
             RemotePortScanner rps = new RemotePortScanner(s, startPort, endPort);
+
             if(InetAddress.getByName(s).isReachable(500)){
                 System.out.println(s);
                 if (rps.getOpenPorts() != null){
                     results.put(s, rps.getOpenPorts());
                 }
             }
+
+            counter++;
         }
     }
 

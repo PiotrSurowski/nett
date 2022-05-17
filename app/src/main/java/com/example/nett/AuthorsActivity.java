58115cc70c5493b4648f35eea8cb467c17842f52
paulinazaw <paulinazawislak1@gmail.com>
2022-05-17 12:14:23 +0200

    making corrections - layouts

---
+++ app/src/main/java/com/example/nett/AuthorsActivity.java
@@ -0,0 +1,15 @@
+package com.example.nett;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AuthorsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authors);
+    }
+}
--- app/src/main/java/com/example/nett/MainActivity.java
+++ app/src/main/java/com/example/nett/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.nett;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Icon;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,7 +13,11 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         ImageButton im_ip = (ImageButton)findViewById(R.id.ip);
         ImageButton im_ports = (ImageButton)findViewById(R.id.ports);
         ImageButton im_hosts = (ImageButton)findViewById(R.id.hosts);
+
 
         final Context context = this;
         im_ip.setOnClickListener(new View.OnClickListener() {
@@ -50,6 +57,33 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menuHome:
+                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menuAuthors:
+                        Toast.makeText(MainActivity.this, "Autorzy", Toast.LENGTH_SHORT).show();
+                        startNewActivity();
+                        break;
+                    case R.id.exit:
+                        Toast.makeText(MainActivity.this, "Wyjście", Toast.LENGTH_SHORT).show();
+                        finish();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
+    private void startNewActivity(){
+        Intent i = new Intent(this, AuthorsActivity.class);
+        startActivity(i);
+        finish();
+    }
 }

8e056c6663b546418b31e36c896c98a6b1291591
piotrsurowski <piotr.surowski248@gmail.com>
2022-05-22 08:30:26 +0100

    Progress bar added to HostsActivity.java and new thread created.

--- app/src/main/java/com/example/nett/HostsActivity.java
+++ app/src/main/java/com/example/nett/HostsActivity.java
@@ -1,12 +1,16 @@
 package com.example.nett;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,7 +18,7 @@ import java.util.List;
 import IPScanner.PortScanner;
 import IPScanner.RemotePortScanner;
 
-public class HostsActivity extends Activity {
+public class HostsActivity extends AppCompatActivity {
 
     EditText inputAddress;
     EditText portStartBox;
@@ -23,6 +27,7 @@ public class HostsActivity extends Activity {
     Button scanButton;
     Button clearButton;
     RemotePortScanner portScanner;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,24 +40,31 @@ public class HostsActivity extends Activity {
         resultBox = (TextView) findViewById(R.id.textview2);
         scanButton = (Button) findViewById(R.id.start2);
         clearButton = (Button) findViewById(R.id.wyczysc2);
+        progressDialog = new ProgressDialog(this);
+
 
         scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String address = inputAddress.getText().toString();
-                Integer portStart = Integer.valueOf(portStartBox.getText().toString());
-                Integer portEnd = Integer.valueOf(portEndBox.getText().toString());
-                portScanner = new RemotePortScanner(address, portStart,portEnd);
-                List<Integer> result = new ArrayList<>(portScanner.getOpenPorts());
-                StringBuilder sb = new StringBuilder();
+                showProgressDialogWithTitle("Proszę czekać");
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String address = inputAddress.getText().toString();
+                        Integer portStart = Integer.valueOf(portStartBox.getText().toString());
+                        Integer portEnd = Integer.valueOf(portEndBox.getText().toString());
+                        portScanner = new RemotePortScanner(address, portStart,portEnd);
+                        List<Integer> result = new ArrayList<>(portScanner.getOpenPorts());
+                        StringBuilder sb = new StringBuilder();
 
-                for (Integer i : result){
-                    sb.append(String.valueOf(i))
-                            .append("\n");
-                }
-
-                resultBox.setText(String.valueOf(sb.toString()));
-
+                        for (Integer i : result){
+                            sb.append(String.valueOf(i))
+                                    .append("\n");
+                        }
+                        hideProgressDialogWithTitle();
+                        resultBox.setText(sb.toString());
+                    }
+                }).start();
             }
         });
 
@@ -65,5 +77,17 @@ public class HostsActivity extends Activity {
                 resultBox.setText("");
             }
         });
+    }
+
+    private void showProgressDialogWithTitle(String substring) {
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage(substring);
+        progressDialog.show();
+    }
+
+    private void hideProgressDialogWithTitle() {
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.dismiss();
     }
 }
